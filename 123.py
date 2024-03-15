b25f540e0d111821bfaf5fde92bef55d8195a243
Kysarii <137830714+Kysarii@users.noreply.github.com>
2024-03-15 18:30:54 +0400

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,14 +1,18 @@
 import re
-with open('7.txt') as file:
-    for line in file:
-        numbers = re.findall(r'\b\d+\b', line)
-        if not numbers:
-            print('Файл закончился')
-            break
-        for num_str in numbers:
-            num = int(num_str, 2) if re.match(r'^0b[01]+$', num_str) else int(num_str)
-            if num <= 8192 and num % 2 == 0:
-                b = bin(abs(num))[2:]
+file = open('7.txt')
+while True:
+    a = file.readline().split()
+    if not a:
+        print('Файл закончился')
+        break
+    for num_str in a:
+        if int(num_str) == int((bin(abs(int(num_str)))[2:])):
+            num = int(num_str, 2)
+        else:
+            num = int(num_str)
+        if num <= 8192 and num % 2 == 0:
+            b = str(bin(abs(num))[2:])
+            if re.fullmatch('[0-1]*000[0-1]*', b):
                 b = re.sub(r'000', '*', b)
                 if b.count("*") == 1:
                     pos = b.find('*') + 1
