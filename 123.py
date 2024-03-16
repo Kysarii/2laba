c07427b68e44f7bb9064b28442be2d1457e09656
Kysarii <137830714+Kysarii@users.noreply.github.com>
2024-03-16 12:29:46 +0400

    Update 123.py

--- 123.py
+++ 123.py
@@ -5,17 +5,16 @@ while True:
     if not a:
         print('Файл закончился')
         break
-    for num_str in a:
-        if int(num_str) == int((bin(abs(int(num_str)))[2:])):
-            num = int(num_str, 2)
+    for num in a:
+        if all('0' <= num_str <= '1' for num_str in num):
+            if int(num) <= 8192 and int(num) % 2 == 0:
+                b = str(bin(abs(int(num)))[2:])
+                if re.fullmatch('[0-1]*000[0-1]*', b):
+                    b = re.sub(r'000', '*', b)
+                    if b.count("*") == 1:
+                        pos = b.find('*') + 1
+                        b = b.replace("*", "")
+                        print("Число исключая серию 000:", b)
+                        print("Номер позиции ", pos)
         else:
-            num = int(num_str)
-        if num <= 8192 and num % 2 == 0:
-            b = str(bin(abs(num))[2:])
-            if re.fullmatch('[0-1]*000[0-1]*', b):
-                b = re.sub(r'000', '*', b)
-                if b.count("*") == 1:
-                    pos = b.find('*') + 1
-                    b = b.replace("*", "")
-                    print("Число исключая серию 000:", b)
-                    print("Номер позиции ", pos)
+            print('Неверный формат числа')
