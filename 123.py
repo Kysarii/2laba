c78a48c7c2f13b61d77cee696f30a8ed567cef34
Kysarii <137830714+Kysarii@users.noreply.github.com>
2024-03-16 13:27:15 +0400

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,20 +1,17 @@
 import re
-file = open('7.txt')
-while True:
-    a = file.readline().split()
-    if not a:
-        print('Файл закончился')
-        break
-    for num in a:
-        if all('0' <= num_str <= '1' for num_str in num):
-            if int(num,2) <= 8192 and int(num,2) % 2 == 0:
-                b = str(num)
-                if re.fullmatch('[0-1]*000[0-1]*', b):
-                    b = re.sub(r'000', '*', b)
-                    if b.count("*") == 1:
-                        pos = b.find('*') + 1
-                        b = b.replace("*", "")
-                        print("Число исключая серию 000:", b)
-                        print("Номер позиции ", pos)
-        else:
-            print('Неверный формат числа')
+with open('7.txt') as file:
+    for line in file:
+        sl = line.split()
+        for num in sl:
+            if all('0' <= num_str <= '1' for num_str in num):
+                if int(num,2) <= 8192 and int(num,2) % 2 == 0:
+                    b = str(num)
+                    if re.fullmatch('[0-1]*000[0-1]*', b):
+                        b = re.sub(r'000', '*', b)
+                        if b.count("*") == 1:
+                            pos = b.find('*') + 1
+                            b = b.replace("*", "")
+                            print("Число исключая серию 000:", b)
+                            print("Номер позиции ", pos)
+            else:
+                print('Неверный формат числа')
